be45d1670ed820142275776d82465b2290b2c89b
Kene881 <kenekene753@gmail.com>
2024-05-09 22:53:33 +0500

    Change view, urls. Add html files

--- sensor/forms.py
+++ sensor/forms.py
@@ -17,3 +17,17 @@ class CarForm(forms.ModelForm):
 
 class UploadFileForm(forms.Form):
     file = forms.FileField(widget=forms.ClearableFileInput(attrs={'class': 'form-control'}))
+
+class CustomLoginForm(forms.AuthenticationForm):
+    service_code = forms.CharField(max_length=100, required=True)
+
+    class Meta:
+        fields = ['username', 'password', 'service_code']
+
+
+class InspectionForm(forms.Form):
+    registration_number = forms.CharField(label='Гос. регистрационный знак', max_length=100, required=True)
+    document_number = forms.CharField(label='Номер техпаспорта', max_length=100, required=True)
+    
+class PhotoUploadForm(forms.Form):
+    photo = forms.ImageField(label='Drop photo')
